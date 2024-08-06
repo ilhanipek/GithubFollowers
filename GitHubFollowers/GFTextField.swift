5dc71fbb41c0702896952a3cc9dd8b4c9a142f12
ilhan ipek <aliilhanipek92@gmail.com>
2024-08-06 14:17:29 +0300

    feat(GFTextField): GFTextField added

---
+++ GitHubFollowers/GFTextField.swift
@@ -0,0 +1,20 @@
+//
+//  GFTextField.swift
+//  GitHubFollowers
+//
+//  Created by ilhan serhan ipek on 6.08.2024.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
