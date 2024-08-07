a6fc2e168427577bff5aa9f6096a29052d5dadfe
ilhan ipek <aliilhanipek92@gmail.com>
2024-08-07 21:48:06 +0300

    feat(GFBodyLabel): Custom body label added

---
+++ GitHubFollowers/GFTitleLabel.swift
@@ -0,0 +1,20 @@
+//
+//  GFTitleLabel.swift
+//  GitHubFollowers
+//
+//  Created by ilhan serhan ipek on 7.08.2024.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
