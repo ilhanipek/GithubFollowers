959e0727e9a5cd82e303c77fafb0ba4b9c039e7f
ilhan ipek <aliilhanipek92@gmail.com>
2024-08-21 04:47:14 +0300

    feat(FollowingClient): func to get following list created

--- GitHubFollowers/NetworkLayer/FollowingClient.swift
+++ GitHubFollowers/NetworkLayer/FollowingClient.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+protocol FollowingProtocol {
+  func getFollowingsList(for username: String, pageNumber: Int, followingCount: Int) async throws -> [Following]
+}
+
+extension HTTPClient: FollowingProtocol {
+  
+  func getFollowingsList(for username: String, pageNumber: Int, followingCount: Int) async throws -> [Following] {
+    let urlRequest = try makeUrlRequest(baseUrl: URL(string: baseUrl)!, path: "/users/\(username)/following?per_page=\(followingCount)&page=\(pageNumber)", httpMethod: .get, queryParameters: nil)
+    return try await processRequest(urlRequest: urlRequest, with: [Following].self)
+  }
+}
